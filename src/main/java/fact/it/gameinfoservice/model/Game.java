8742ebc5a4f80dd86730f8de4c5f737167314dba
unknown <r0746415@student.thomasmore.be>
2021-10-08 12:02:10 +0200

    model

--- src/main/java/fact/it/gameinfoservice/model/Game.java
+++ src/main/java/fact/it/gameinfoservice/model/Game.java
@@ -1,4 +1,48 @@
 package fact.it.gameinfoservice.model;
 
+import java.util.Date;
+
 public class Game {
+    private String name;
+    private int release_year;
+    private String developer;
+    private int sales;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(int release_year) {
+        this.release_year = release_year;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
+
+    public Game() {
+
+    }
 }
+
+
